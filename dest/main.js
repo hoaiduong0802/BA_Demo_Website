10c697c5f899bb3cb54bb5fadb0768277d4c9d36
abc <abc@example.com>
2024-07-09 19:43:36 +0700

    set-up workflow

--- dest/main.js
+++ dest/main.js
@@ -1,1 +1,20 @@
-console.log('Hello CFD Circle');
+const swiper = new Swiper(".swiper", {
+  speed: 1000,
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
