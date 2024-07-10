690f952f0c686ae119aea0f78fd04b0ab7df170e
Huỳnh Duy Phúc <83865270+HuynhDuyPhuc201@users.noreply.github.com>
2024-07-10 21:44:05 +0700

    update project

--- dest/main.js
+++ dest/main.js
@@ -18,3 +18,39 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+var elem = document.querySelector(".combotHot .combotHot_wrap");
+var flkty = new Flickity(elem, {
+  cellAlign: "left",
+  prevNextButtons: true,
+  contain: true,
+  wrapAround: true,
+  pageDots: false,
+  selectedAttraction: 0.04,
+  groupCells: true,
+});
+
+var elem = document.querySelector(".beautiful .beautiful_wrap");
+var flkty = new Flickity(elem, {
+  cellAlign: "left",
+  prevNextButtons: false,
+  contain: true,
+  wrapAround: true,
+  pageDots: true,
+  selectedAttraction: 0.04,
+  groupCells: true,
+});
+
+const btnSideBar = document.querySelector(".menuSideBar");
+btnSideBar.addEventListener("click", (e) => {
+  document.querySelector(".sliderBar").classList.toggle("openSidebar");
+  btnSideBar.classList.toggle("open");
+});
+
+const categoryItem = document.querySelector(".AllProduct_category ul li");
+categoryItem.addEventListener("click", (e) => {
+  const clicked = e.target;
+  document
+    .querySelector(".AllProduct_category .dropdown")
+    .classList.toggle("active");
+});
