34edb262d100f509286e0531b9c5c57340c048f2
Huỳnh Duy Phúc <83865270+HuynhDuyPhuc201@users.noreply.github.com>
2024-07-11 15:05:02 +0700

    detail project

--- dest/main.js
+++ dest/main.js
@@ -30,8 +30,8 @@ var flkty = new Flickity(elem, {
   groupCells: true,
 });
 
-var elem = document.querySelector(".beautiful .beautiful_wrap");
-var flkty = new Flickity(elem, {
+var elem2 = document.querySelector(".beautiful .beautiful_wrap");
+var flkty = new Flickity(elem2, {
   cellAlign: "left",
   prevNextButtons: false,
   contain: true,
@@ -41,16 +41,42 @@ var flkty = new Flickity(elem, {
   groupCells: true,
 });
 
+var elem1 = document.querySelector(".detailProduct_slider");
+var flkty = new Flickity(elem1, {
+  cellAlign: "left",
+  prevNextButtons: false,
+  contain: true,
+  wrapAround: true,
+  pageDots: true,
+  selectedAttraction: 0.04,
+  // groupCells: true,
+  fullscreen: true,
+  lazyLoad: 1,
+});
+
 const btnSideBar = document.querySelector(".menuSideBar");
-btnSideBar.addEventListener("click", (e) => {
+btnSideBar?.addEventListener("click", (e) => {
   document.querySelector(".sliderBar").classList.toggle("openSidebar");
   btnSideBar.classList.toggle("open");
 });
 
 const categoryItem = document.querySelector(".AllProduct_category ul li");
-categoryItem.addEventListener("click", (e) => {
-  const clicked = e.target;
+categoryItem?.addEventListener("click", (e) => {
   document
     .querySelector(".AllProduct_category .dropdown")
     .classList.toggle("active");
+});
+
+const tabLi = document.querySelectorAll(".description__tabs li");
+const tabItem = document.querySelectorAll(".description__item");
+
+tabLi.forEach((item, index) => {
+  const prevItem = tabItem[(index + tabItem.length - 1) % tabItem.length];
+  const prevTab = tabLi[(index + tabLi.length - 1) % tabLi.length];
+  item.addEventListener("click", (e) => {
+    prevTab.classList.remove("active");
+    prevItem.classList.remove("active");
+    item.classList.add("active");
+    tabItem[index].classList.add("active");
+  });
 });
